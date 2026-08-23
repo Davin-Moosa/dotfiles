e2c139cb673a9373a0c5077e8c45c29fce5e1f59
Davin Moosa <davinmoosa@proton.me>
2026-08-23 18:38:06 +0200

    KDE

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,9 @@ if status is-interactive
 # Commands to run in interactive sessions can go here
 end
 
+function fish_greeting
+end
+
 function man
   command man $argv | bat -pl man
 end
--- setup.sh
+++ setup.sh
@@ -3,22 +3,13 @@
 # symlinks
 dotdir="$HOME/dotfiles"
 confs=(
-  "Proton"
-  "fish"
-  "git"
-  "mako"
-  "niri"
-  "nvim"
-  "qt5ct"
-  "qt6ct"
-  "sunsetr"
-  "waybar"
-  "wezterm"
-  "wofi"
+  Proton
+  fish
+  git
+  nvim
 )
-if [ -d "$dotdir" ] then
+if [ -d "$dotdir" ]; then
   for conf in "${confs[@]}"; do
-    rm -rf "$HOME/.config/$conf"
     ln -sf "$dotdir/config/$conf" "$HOME/.config/"
   done
 else
